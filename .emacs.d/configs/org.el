36c7034272e87668e8ed48da9c853f6d08e3fe01
Mihai Rinzis <m.rinzis@gmail.com>
2014-07-11 10:11:21 +0300

    Add org.el

---
+++ .emacs.d/configs/org.el
@@ -0,0 +1,33 @@
+;;; org.el --- Org-mode config
+
+;;; Commentary:
+;;;
+
+;;; Code:
+
+;; display images
+(setq org-startup-with-inline-images (display-graphic-p))
+
+;; track times
+(setq org-clock-persist 'history)
+(org-clock-persistence-insinuate)
+
+;; curate this
+(setq org-hide-leading-stars t
+      org-src-fontify-natively t
+      org-startup-indented t
+      org-completion-use-iswitchb t
+      org-log-done t
+      org-email-link-description-format "Email %c (%d): %s"
+      org-agenda-include-diary t
+      org-enforce-todo-dependencies t
+      ;; org clock
+      org-clock-persist-query-save t
+      org-clock-persistence-insinuate t
+      org-clock-persist t
+      org-clock-in-resume t
+      org-clock-in-switch-to-state "STARTED"
+      org-time-clocksum-format
+      '(:hours "%d" :require-hours t :minutes ":%02d" :require-minutes t))
+
+;;; org.el ends here
