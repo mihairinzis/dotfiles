0ae9cb437679f23a0b6395c9af5e13a25afe3c15
Mihai Rinzis <m.rinzis@gmail.com>
2014-08-03 14:34:13 +0300

    Add appearance in init.el

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -14,5 +14,7 @@
 (load-file (expand-file-name "misc.el" emacsd-configs-dir))
 (load-file (expand-file-name "dired.el" emacsd-configs-dir))
 (load-file (expand-file-name "ibuffer.el" emacsd-configs-dir))
+(load-file (expand-file-name "appearance.el" emacsd-configs-dir))
+
 
 ;;; init.el ends here
