8ea03ae49b970cc6ecd715c5bed2d57e7c25cc64
Mihai Rinzis <m.rinzis@gmail.com>
2014-08-30 21:49:24 +0300

    Increase dmenu font size

--- .config/scripts/dmenu-bind.sh
+++ .config/scripts/dmenu-bind.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-exe=`dmenu_path | dmenu_run -b -nb '#3f3f3f' -nf '#dcdccc' -sb '#dcdccc' -sf '#151617'` && eval "exec $exe"
+exe=`dmenu_path | dmenu_run -b -nb '#3f3f3f' -nf '#dcdccc' -sb '#dcdccc' -sf '#151617' -fn "-xos4-terminus-medium-r-*-*-18g-*"` && eval "exec $exe"
