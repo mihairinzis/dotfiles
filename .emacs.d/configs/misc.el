9c2be3ecba5f4278ab192f63a9426c3e51011ff1
Mihai Rinzis <m.rinzis@gmail.com>
2014-12-11 09:56:37 +0200

    use ido for buffer switching

--- .emacs.d/configs/misc.el
+++ .emacs.d/configs/misc.el
@@ -232,6 +232,7 @@
 ;; (helm-mode 1)
 ;; (define-key global-map (kbd "C-x C-f") 'helm-find-files)
 ;; (define-key global-map (kbd "M-x") 'helm-M-x)
+(define-key global-map (kbd "C-x b") 'ido-switch-buffer)
 
 ;; (progn
 ;; (require 'helm-config)
