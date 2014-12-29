5d57b9754fff5778a8c0d746758f8c8c1f517203
Mihai Rinzis <m.rinzis@gmail.com>
2014-12-29 22:47:41 +0200

    Make gpg use public key

--- backup.sh
+++ backup.sh
@@ -3,5 +3,6 @@
 tar pzchf ~/backup-$(date +%Y-%m-%d).tar.gz * \
     --exclude=temp
 
-gpg --symmetric --cipher-algo AES256 ~/backup-$(date +%Y-%m-%d).tar.gz
+# gpg --symmetric --cipher-algo AES256 ~/backup-$(date +%Y-%m-%d).tar.gz
+gpg --encrypt --recipient 259A02CB ~/backup-$(date +%Y-%m-%d).tar.gz
 rm -f ~/backup-$(date +%Y-%m-%d).tar.gz
