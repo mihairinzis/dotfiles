8e851364128b7509276000ded81901414020440a
Mihai Rinzis <m.rinzis@gmail.com>
2015-01-01 21:51:55 +0200

    Change multiple cursors prefix to C-c

--- .emacs.d/configs/misc.el
+++ .emacs.d/configs/misc.el
@@ -71,10 +71,10 @@
 
 ;; Multiple cursors
 (require 'multiple-cursors)
-(global-set-key (kbd "C-z m l") 'mc/edit-lines)
-(global-set-key (kbd "C-z m n") 'mc/mark-next-like-this)
-(global-set-key (kbd "C-z m p") 'mc/mark-previous-like-this)
-(global-set-key (kbd "C-z m a") 'mc/mark-all-like-this)
+(global-set-key (kbd "C-c m l") 'mc/edit-lines)
+(global-set-key (kbd "C-c m n") 'mc/mark-next-like-this)
+(global-set-key (kbd "C-c m p") 'mc/mark-previous-like-this)
+(global-set-key (kbd "C-c m a") 'mc/mark-all-like-this)
 (global-set-key (kbd "C-S-<mouse-1>") 'mc/add-cursor-on-click)
 ;; (global-set-key (kbd "C-x r t") 'inline-string-rectangle)
 
