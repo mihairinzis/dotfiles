664e28b72e5021a6d0b754558ca6977a4e608d36
Mihai Rinzis <m.rinzis@gmail.com>
2015-01-06 14:42:53 +0200

    Refactor utf8 emacs misc

--- .emacs.d/configs/misc.el
+++ .emacs.d/configs/misc.el
@@ -43,11 +43,11 @@
 
 ;; utf everywhere
 (set-language-environment 'UTF-8)
-(setq locale-coding-system 'utf-8)
+(setq locale-coding-system 'utf-8
+      buffer-file-coding-system 'utf-8-unix)
 (set-terminal-coding-system 'utf-8)
 (set-keyboard-coding-system 'utf-8)
 (prefer-coding-system 'utf-8)
-(setq default-buffer-file-coding-system 'utf-8-unix)
 
 ;; ispell
 ;; find aspell and hunspell automatically
