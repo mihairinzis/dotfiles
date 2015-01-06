4d06919d541fc75aa39389c1a86c32cdf0b3adff
Mihai Rinzis <m.rinzis@gmail.com>
2015-01-06 16:16:35 +0200

    Add jabber to ibuffer

--- .emacs.d/configs/ibuffer.el
+++ .emacs.d/configs/ibuffer.el
@@ -85,19 +85,6 @@
      ))
 
 
-;; Modify the default ibuffer-formats
-(setq ibuffer-formats
-      '((mark modified read-only vc-status-mini " "
-              (name 18 18 :left :elide)
-              " "
-              (size-h 9 -1 :right)
-              " "
-              (mode 16 16 :left :elide)
-              " "
-              (vc-status 16 16 :left)
-              " "
-              filename-and-process)))
-
 (setq ibuffer-filter-group-name-face 'font-lock-doc-face)
 
 (global-set-key (kbd "C-x C-b") 'ibuffer)
