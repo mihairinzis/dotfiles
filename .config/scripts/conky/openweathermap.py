7ea512b339668bca66d3709e610112e2396bb344
Mihai Rinzis <mihai@rmi.catalysts>
2017-08-09 14:20:10 +0300

    conky weather fix

--- .config/scripts/conky/openweathermap.py
+++ .config/scripts/conky/openweathermap.py
@@ -1,6 +1,6 @@
 import json
 import urllib.request
-req = urllib.request.Request('http://api.openweathermap.org/data/2.5/weather?APPID=f5296d5dee9921516fddf8c2ac41e794&q=Cluj&units=metric')
+req = urllib.request.Request('http://api.openweathermap.org/data/2.5/weather?APPID=16e897b28f908604765b6e22237ca1de&q=Cluj&units=metric')
 with urllib.request.urlopen(req) as response:
-    result = json.loads(response.readall().decode('utf-8'))
+    result = json.loads(response.read().decode('utf-8'))
     print(str(int(result['main']['temp'])) + ' ' + result['weather'][0]['description'])
