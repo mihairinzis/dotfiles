c8ed97ea6e4dc8940023b10779d3ab744f634305
Mihai Rinzis <m.rinzis@gmail.com>
2017-11-16 12:55:53 +0200

    fix weather city request

--- .config/scripts/conky/openweathermap.py
+++ .config/scripts/conky/openweathermap.py
@@ -1,6 +1,14 @@
 import json
-import urllib.request
-req = urllib.request.Request('http://api.openweathermap.org/data/2.5/weather?APPID=16e897b28f908604765b6e22237ca1de&q=Cluj&units=metric')
-with urllib.request.urlopen(req) as response:
+from urllib.request import Request, urlopen
+from urllib.parse import urlencode
+
+params = urlencode({
+    'appid':'16e897b28f908604765b6e22237ca1de',
+    'q': 'Cluj-Napoca',
+    'units': 'metric',
+    'lang': 'ro'
+})
+req = Request('http://api.openweathermap.org/data/2.5/weather?' + params)
+with urlopen(req) as response:
     result = json.loads(response.read().decode('utf-8'))
     print(str(int(result['main']['temp'])) + ' ' + result['weather'][0]['description'])
