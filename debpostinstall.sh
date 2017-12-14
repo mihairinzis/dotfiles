bfc6524ba9b3de05a9c10063bf5e69382dcca56d
Mihai Rinzis <m.rinzis@gmail.com>
2017-12-14 14:13:07 +0200

    add chrome to post install

--- debpostinstall.sh
+++ debpostinstall.sh
@@ -7,3 +7,9 @@ fi
 # install youtube-dl
 curl -L https://yt-dl.org/downloads/latest/youtube-dl -o /usr/local/bin/youtube-dl
 chmod a+rx /usr/local/bin/youtube-dl
+
+# install google-chrome
+wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
+sudo sh -c 'echo "deb https://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
+sudo apt-get update
+sudo apt-get install google-chrome-stable
