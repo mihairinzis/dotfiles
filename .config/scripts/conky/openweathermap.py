41794af8ee60c675c7daa00fe2d0c095e2af14e7
Mihai Rinzis <m.rinzis@gmail.com>
2018-09-10 08:06:36 +0300

    various polybar changes

--- .config/scripts/conky/openweathermap.py
+++ .config/scripts/conky/openweathermap.py
@@ -1,6 +1,7 @@
 import json
 from urllib.request import Request, urlopen
 from urllib.parse import urlencode
+from urllib.error import URLError
 
 params = urlencode({
     'appid':'16e897b28f908604765b6e22237ca1de',
@@ -9,6 +10,9 @@ params = urlencode({
     'lang': 'ro'
 })
 req = Request('http://api.openweathermap.org/data/2.5/weather?' + params)
-with urlopen(req) as response:
-    result = json.loads(response.read().decode('utf-8'))
-    print(str(int(result['main']['temp'])) + ' ' + result['weather'][0]['description'])
+try:
+    with urlopen(req) as response:
+        result = json.loads(response.read().decode('utf-8'))
+        print(str(int(result['main']['temp'])) + ' ' + result['weather'][0]['description'])
+except URLError:
+    print('Could not fetch the weather')
